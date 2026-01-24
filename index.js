26cf7fdabc0c743e3bf50e3172c632114b2efbb0
XPite56 <daniilgrigorov26@gmail.com>
2026-01-24 17:00:14 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-export default async ({ req, res, log }) => {
-    if (req.body && req.body.text) {
-        log(`Уведомление для: ${req.body.receiver}`);
-        // Тут серверная магия отправки уведомления
-    }
-    return res.json({ ok: true });
-};
